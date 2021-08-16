fd10a9906b83e26f0960496f702fcbe6d6d7f320
elat819 <83850638+elat819@users.noreply.github.com>
2021-08-15 22:22:00 -0500

    Update GetSentimentScore3.py

--- GetSentimentScore3.py
+++ GetSentimentScore3.py
@@ -1,1 +1,25 @@
+from google.cloud import automl
 
+# TODO(developer): Uncomment and set the following variables
+# project_id = "YOUR_PROJECT_ID"
+# model_id = "YOUR_MODEL_ID"
+# file_path = "path_to_local_file.txt"
+
+prediction_client = automl.PredictionServiceClient()
+
+# Get the full path of the model.
+model_full_id = automl.AutoMlClient.model_path("gcp-project-319802", "us-central1", "TST2306007935957860352")
+
+# Read the file content for translation.
+#with open(file_path, "rb") as content_file:
+with open("Test_Files/TestInput.csv", "rb") as content_file:
+    content = content_file.read()
+content.decode("utf-8")
+
+text_snippet = automl.TextSnippet(content=content)
+payload = automl.ExamplePayload(text_snippet=text_snippet)
+
+response = prediction_client.predict(name=model_full_id, payload=payload)
+translated_content = response.payload[0].translation.translated_content
+
+print(u"Translated content: {}".format(translated_content.content))
